9ea32f00a145b87668a2cb0d922924d40bdb63e1
Hector Flores <hecto932@gmail.com>
2016-10-05 01:07:02 -0500

    Inicializando el proyecto con NPM

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ var app = express();
 app.get('/', function(req, res){
 	res.send("Hello world!");
 });
+
+app.listen(3000, function(err){
+	if(err) return console.log("Hubo un error"), process.exit(1);
+	console.log("Escuchando en el puerto 3000");
+});

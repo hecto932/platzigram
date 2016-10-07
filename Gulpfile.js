616d6b993400e7a903dd407048ae870cde6eb73b
Hector Flores <hecto932@gmail.com>
2016-10-07 01:56:54 -0500

    Agregando Javascript en el cliente

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var rename = require("gulp-rename");
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('styles', function(){
 	gulp
@@ -14,6 +17,15 @@ gulp.task('assets', function(){
 	gulp
 		.src('assets/*')
 		.pipe(gulp.dest('public'))
+});
+
+gulp.task('scripts', function(){
+	browserify('./src/index.js') //Here we can process this file with broswerify
+		.transform(babel) // parse the code using babel EMACS 2015
+		.bundle() // Recived the bundle
+		.pipe(source('index.js'))
+		.pipe(rename('app.js'))
+		.pipe(gulp.dest('public'));
 })
 
-gulp.task('default', ['styles', 'assets']);
+gulp.task('default', ['styles', 'assets', 'scripts']);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+var numeros = [1,2,3,4,5,6,7,8,9,0];
+
+var numerosMas1 = numeros.map(n => n + 1);
+
+console.log(numerosMas1);

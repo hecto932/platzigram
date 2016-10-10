ca95d71a7e92ff1801e3d4cbff2728f6c6a85403
Hector Flores <hecto932@gmail.com>
2016-10-09 19:31:12 -0500

    Agregando la funcionalidad de Like

--- public/app.js
+++ public/app.js
@@ -2633,23 +2633,40 @@ module.exports = function layout(content) {
 },{"yo-yo":8}],22:[function(require,module,exports){
 var yo = require('yo-yo');
 
-module.exports = function (pic) {
-  return yo`<div class="card">
+module.exports = function pirctureCard(pic) {
+
+  var el;
+
+  function render(picture) {
+    return yo`<div class="card ${ picture.liked ? 'liked' : '' }">
     <div class="card-image">
-      <img class="activator" src=${ pic.url }">
+      <img class="activator" src=${ picture.url }">
     </div>
     <div class="card-content">
-      <a href="/user/${ pic.user.username }" class="card-title">
-      	<img src="${ pic.user.avatar }" class="avatar" />
-      	<span class="username">${ pic.user.username }</span>
+      <a href="/user/${ picture.user.username }" class="card-title">
+        <img src="${ picture.user.avatar }" class="avatar" />
+        <span class="username">${ picture.user.username }</span>
       </a>
       <small class="right time">Hace 1 dia</small>
       <p>
-		<a href="#" class="left"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-      	<span class="left likes">${ pic.likes } me gusta</span>
+        <a href="#" onclick=${ like.bind(null, true) } class="left"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+        <a href="#" onclick=${ like.bind(null, false) } class="left"><i class="fa fa-heart" aria-hidden="true"></i></a>
+        <span class="left likes">${ picture.likes } me gusta</span>
       </p>
     </div>
   </div>`;
+  }
+
+  function like(liked) {
+    pic.liked = liked;
+    pic.likes += liked ? 1 : -1;
+    var newEl = render(pic);
+    yo.update(el, newEl);
+    return false;
+  }
+
+  el = render(pic);
+  return el;
 };
 
 },{"yo-yo":8}],23:[function(require,module,exports){

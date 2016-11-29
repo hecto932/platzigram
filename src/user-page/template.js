e827923a045c76e98ac272bde21d500f7dfe1b1a
Hector Flores <hecto932@gmail.com>
2016-11-29 00:38:35 -0600

    Implementacion de perfil de usuario

--- src/user-page/template.js
+++ src/user-page/template.js
@@ -11,8 +11,8 @@ module.exports = function (user) {
             <img src="${user.avatar}" alt="${user.username}" class="responsive-img circle" />
           </div>
           <div class="col s12 m10 offset-m1 l6 left-align">
-            <h2 class="hide-on-large-only center-align">${user.username}</h2>
-            <h2 class="hide-on-med-and-down left-align">${user.username}</h2>
+            <h2 class="hide-on-large-only center-align">${user.name}</h2>
+            <h2 class="hide-on-med-and-down left-align">${user.name}</h2>
           </div>
         </div>
       </div>
@@ -20,15 +20,15 @@ module.exports = function (user) {
         ${user.pictures.map(function (picture) {
           return yo`<div class="col s12 m6 l4">
             <a href="/${user.username}/${picture.id}" class="picture-container">
-              <img src="${picture.src})" class="picture" />
-              <div class="likes"><i class="fa fa-heart"></i> ${picture.likes}</div>
+              <img src="${picture.src}" class="picture" />
+              <div class="likes"><i class="fa fa-heart"></i> ${picture.likes || 0 }</div>
             </a>
             <div id="modal${picture.id}" class="modal modal-fixed-footer">
               <div class="modal-content center">
-                <img src="${picture.src})" />
+                <img src="${picture.src}" />
               </div>
               <div class="modal-footer">
-                <div class="btn btn-flat likes"><i class="fa fa-heart"></i> ${translate('likes', { likes: picture.likes })}</div>
+                <div class="btn btn-flat likes"><i class="fa fa-heart"></i> ${translate('likes', { likes: picture.likes || 0 })}</div>
               </div>
             </div>
           </div>`;
